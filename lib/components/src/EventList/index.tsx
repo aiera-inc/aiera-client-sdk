d37cf09a7a95bd9b6df7751d0856266cc9201d66
Kevin LaFlamme <kevin@aiera.com>
2021-07-21 10:56:42 -0400

    Add shared Event type and EventList click handler

--- lib/components/src/EventList/index.tsx
+++ lib/components/src/EventList/index.tsx
@@ -1,21 +1,20 @@
-import { FC, ReactElement } from 'react';
-
-interface Event {
-    id: string;
-    title: string;
-    ticker: string;
-}
+import { FC, ReactElement, MouseEvent } from 'react';
+import { Event } from 'types';
 
 interface Props {
     events: Event[];
+    onSelectEvent?: (event: MouseEvent) => void;
 }
 
-export const EventList: FC<Props> = (props: Props): ReactElement => (
-    <ul>
-        {props.events.map((event) => (
-            <li key={event.id}>
-                {event.ticker} - {event.title}
-            </li>
-        ))}
-    </ul>
-);
+export const EventList: FC<Props> = (props: Props): ReactElement => {
+    const { events, onSelectEvent } = props;
+    return (
+        <ul>
+            {events.map((event) => (
+                <li onClick={onSelectEvent} key={event.id}>
+                    {event.ticker} - {event.title}
+                </li>
+            ))}
+        </ul>
+    );
+};
---
+++ lib/components/src/types.ts
@@ -0,0 +1,5 @@
+export interface Event {
+    id: string;
+    title: string;
+    ticker: string;
+}
