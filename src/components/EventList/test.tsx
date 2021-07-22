1199cc86f184abe4fbac306cd4434487ae3b264c
Kevin LaFlamme <kevinl@hey.com>
2021-07-21 21:25:52 -0400

    Add test for event click

--- src/components/EventList/test.tsx
+++ src/components/EventList/test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import { EventList } from '.';
 
 describe('EventList', () => {
@@ -8,8 +8,12 @@ describe('EventList', () => {
         screen.getByText('No events.');
     });
 
-    test('renders an empty state', () => {
-        render(<EventList events={[{ id: '1', title: 'Event Title', ticker: 'AIERA' }]} />);
-        screen.getByText('AIERA - Event Title');
+    test('renders an event list and selects and event on click', () => {
+        const onSelectEvent = jest.fn();
+        render(
+            <EventList events={[{ id: '1', title: 'Event Title', ticker: 'AIERA' }]} onSelectEvent={onSelectEvent} />
+        );
+        fireEvent(screen.getByText('AIERA - Event Title'), new MouseEvent('click', { bubbles: true }));
+        expect(onSelectEvent).toHaveBeenCalled();
     });
 });
