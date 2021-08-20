c90344d9fe93ff7f2c5ab7c0477591f96f0fd8e4
Kevin LaFlamme <kevin@aiera.com>
2021-08-20 09:42:17 -0400

    Better live reload

--- src/dev/build.ts
+++ src/dev/build.ts
@@ -1,7 +1,8 @@
 /* eslint  @typescript-eslint/unbound-method: 0 */
 // import path from 'path';
+import http, { IncomingMessage, ServerResponse } from 'http';
 import ts from 'typescript';
-import { build } from 'esbuild';
+import { build, serve } from 'esbuild';
 import copy from 'cpy';
 import yargs from 'yargs/yargs';
 import chokidar from 'chokidar';
@@ -51,12 +52,58 @@ interface Arguments {
     watch: boolean;
 }
 
+function proxy(fromPort: number, toPort: number, onReloadReq: (res: ServerResponse) => void) {
+    function onRequest(req: IncomingMessage, res: ServerResponse) {
+        if (req.url === '/reload') {
+            onReloadReq(res);
+            return;
+        }
+        const proxied = http.request(
+            {
+                hostname: 'localhost',
+                port: toPort,
+                path: `/dev${req.url || ''}`,
+                method: req.method,
+                headers: req.headers,
+            },
+            (proxyRes) => {
+                res.writeHead(proxyRes.statusCode || 200, proxyRes.headers);
+                proxyRes.pipe(res, { end: true });
+            }
+        );
+        req.pipe(proxied, { end: true });
+    }
+    http.createServer(onRequest).listen(fromPort);
+}
+
 async function cli(args: Arguments) {
     if (args.watch) {
-        void buildAll();
+        await buildAll();
+
+        let currentRes: ServerResponse | undefined;
+        proxy(8000, 8001, (res) => {
+            currentRes = res;
+        });
         chokidar.watch('src/**/*', { ignoreInitial: true }).on('all', () => {
             void buildAll();
+            currentRes?.end();
         });
+        void serve(
+            {
+                port: 8001,
+                servedir: 'build',
+                onRequest: ({ method, path, timeInMS }) => {
+                    console.log(`${method} ${path} ${timeInMS}ms`);
+                },
+            },
+            {
+                bundle: true,
+                outdir: 'build/dev/bundle',
+                tsconfig: 'tsconfig.bundle.json',
+                entryPoints: ['build/dev/index.js'],
+                write: false,
+            }
+        );
     } else {
         await buildAll();
     }
