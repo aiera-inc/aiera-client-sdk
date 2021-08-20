f269a96003a19b0d87232c6bf6681192db44a7b7
Kevin LaFlamme <kevin@aiera.com>
2021-08-20 19:22:35 -0400

    Oops, commit the types too

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,6 +1,28 @@
+import { SyntheticEvent } from 'react';
 import { CombinedError } from 'urql';
 
 export type Nullable<T> = {
     [P in keyof T]?: T[P] | null;
 };
 export type QueryError = CombinedError;
+
+export interface ChangeEvent<T = unknown> {
+    /**
+     * The name for this input, used so that a single change handler can be used
+     *for many inputs.
+     */
+    name?: string;
+    /**
+     * Shortcut to get the value from the input.
+     */
+    value: T;
+}
+/**
+ * Implements a consistent change handler interface for all inputs that
+ * is backwards compatible with browser change handler but has an optional
+ * second arg with some shortcuts for common use cases.
+ */
+export type ChangeHandler<T = unknown, E extends SyntheticEvent = SyntheticEvent> = (
+    event: E,
+    change: ChangeEvent<T>
+) => void;
