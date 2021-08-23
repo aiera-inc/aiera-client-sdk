6561c3b2f87938bc145866814574ada89d4a7367
Kevin LaFlamme <kevin@aiera.com>
2021-08-23 14:26:16 -0400

    Initial WIP event list loaded from server

--- src/modules/EventList/index.tsx
+++ src/modules/EventList/index.tsx
@@ -1,23 +1,133 @@
-import React, { FC, ReactElement, MouseEvent } from 'react';
-import { Event } from 'types/generated';
+import React, { ReactElement, MouseEvent, useState } from 'react';
+import gql from 'graphql-tag';
+import { useQuery } from 'urql';
+
+import { ChangeHandler } from 'types';
+import { Tabs } from 'components';
+import { EventListQuery } from 'types/generated';
+import { FilterBy } from './FilterBy';
 import './styles.css';
 
-export interface Props {
-    events?: Event[];
+enum EventListType {
+    live,
+    recent,
+}
+
+enum FilterByType {
+    transcript,
+    earningsOnly,
+}
+
+export interface EventListUIProps {
+    events?: EventListQuery['events'];
+    filterByTypes?: FilterByType[];
+    listType?: EventListType;
+    loading?: boolean;
+    onSelectFilterBy?: ChangeHandler<FilterByType[]>;
+    onSelectListType?: ChangeHandler<EventListType>;
     onSelectEvent?: (event: MouseEvent) => void;
 }
 
-export const EventList: FC<Props> = (props: Props): ReactElement => {
-    const { events, onSelectEvent } = props;
-    return events && events.length ? (
-        <ul>
-            {events.map((event) => (
-                <li onClick={onSelectEvent} key={event.id}>
-                    {event.title}
-                </li>
-            ))}
-        </ul>
-    ) : (
-        <span>No events.</span>
+export const EventListUI = (props: EventListUIProps): ReactElement => {
+    const { events, filterByTypes, listType, loading, onSelectFilterBy, onSelectListType, onSelectEvent } = props;
+    return (
+        <div className="overflow-y-scroll eventlist">
+            <div className="sticky top-0 flex items-center h-16 p-3 bg-gray-200 eventlist__header">
+                <input className="w-3/4 p-2 text-sm rounded-lg" placeholder="Search Events and Transcripts" />
+            </div>
+            <div className="flex flex-col flex-grow p-2 eventlist__tabs">
+                <div>
+                    <Tabs<EventListType>
+                        onChange={onSelectListType}
+                        options={[
+                            { value: EventListType.live, label: 'Live & Upcoming' },
+                            { value: EventListType.recent, label: 'Recent Events' },
+                        ]}
+                        value={listType}
+                    />
+                </div>
+                <div className="sticky pt-2 bg-white top-16">
+                    <FilterBy
+                        onChange={onSelectFilterBy}
+                        options={[
+                            { value: FilterByType.transcript, label: 'Has transcript' },
+                            { value: FilterByType.earningsOnly, label: 'Earnings only' },
+                        ]}
+                        value={filterByTypes}
+                    />
+                </div>
+                <div>
+                    {loading ? (
+                        <div>Loading...</div>
+                    ) : events && events.length ? (
+                        <ul>
+                            {events.map((event) => (
+                                <li className="text-sm" onClick={onSelectEvent} key={event.id}>
+                                    <div className="flex flex-row">
+                                        <div className="flex items-center justify-center p-2">
+                                            <div className="flex items-center justify-center w-10 h-10 bg-gray-400 rounded-full" />
+                                        </div>
+                                        <div className="flex flex-col justify-center flex-1 p-2 ">
+                                            <div>
+                                                <span className="pr-1 font-semibold">AMZN</span>
+                                                <span className="text-gray-200">NYSE</span>
+                                                <span className="text-gray-400"> • {event.eventType}</span>
+                                            </div>
+                                            <div>{event.title}</div>
+                                        </div>
+                                        <div className="flex flex-col items-center justify-center text-xs">
+                                            <div>March 3rd</div>
+                                            <div>5:30 PM 2021</div>
+                                        </div>
+                                    </div>
+                                </li>
+                            ))}
+                        </ul>
+                    ) : (
+                        <span>No events.</span>
+                    )}
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export interface EventListProps {
+    onSelectEvent?: (event: MouseEvent) => void;
+}
+
+interface EventListState {
+    filterByTypes: FilterByType[];
+    listType: EventListType;
+}
+
+export const EventList = (props: EventListProps): ReactElement => {
+    const { onSelectEvent } = props;
+    const [state, setState] = useState<EventListState>({
+        filterByTypes: [],
+        listType: EventListType.live,
+    });
+    const [eventListResult] = useQuery<EventListQuery>({
+        query: gql`
+            query EventList {
+                events(eventIds: []) {
+                    id
+                    title
+                    eventDate
+                    eventType
+                }
+            }
+        `,
+    });
+    return (
+        <EventListUI
+            events={eventListResult.data?.events}
+            filterByTypes={state.filterByTypes}
+            listType={state.listType}
+            loading={eventListResult.fetching}
+            onSelectFilterBy={(_, { value }) => setState({ ...state, filterByTypes: value })}
+            onSelectListType={(_, { value }) => setState({ ...state, listType: value })}
+            onSelectEvent={onSelectEvent}
+        />
     );
 };
--- src/modules/EventList/test.tsx
+++ src/modules/EventList/test.tsx
@@ -1,18 +1,58 @@
 import React from 'react';
 import { fireEvent, render, screen } from '@testing-library/react';
-import { Event } from 'types/generated';
-import { EventList } from '.';
+import { fromValue } from 'wonka';
 
-describe('EventList', () => {
+import { renderWithClient } from 'testUtils';
+import { Event } from 'types/generated';
+import { EventList, EventListUI } from '.';
+
+describe('EventListUI', () => {
+    test('renders a loading state', () => {
+        render(<EventListUI loading />);
+        screen.getByText('Loading...');
+    });
+
     test('renders an empty state', () => {
-        render(<EventList />);
+        render(<EventListUI />);
         screen.getByText('No events.');
     });
 
     test('renders an event list and selects and event on click', () => {
         const onSelectEvent = jest.fn();
-        render(<EventList events={[{ id: '1', title: 'Event Title' } as Event]} onSelectEvent={onSelectEvent} />);
+        render(<EventListUI events={[{ id: '1', title: 'Event Title' } as Event]} onSelectEvent={onSelectEvent} />);
         fireEvent(screen.getByText('Event Title'), new MouseEvent('click', { bubbles: true }));
         expect(onSelectEvent).toHaveBeenCalled();
+    });
+});
+
+describe('EventList', () => {
+    test('handles loading state', () => {
+        renderWithClient(<EventList />);
+        screen.getByText('Loading...');
+    });
+
+    test('handles empty state', () => {
+        renderWithClient(<EventList />, {
+            executeQuery: () =>
+                fromValue({
+                    data: {
+                        events: [],
+                    },
+                }),
+        });
+        screen.getByText('No events.');
+    });
+
+    test('handles event list', () => {
+        renderWithClient(<EventList />, {
+            executeQuery: () =>
+                fromValue({
+                    data: {
+                        events: [{ id: '1', title: 'Event Title', eventType: 'earnings', eventDate: '' }],
+                    },
+                }),
+        });
+        screen.getByText('Event Title');
+        screen.getByText(/earnings$/);
     });
 });
