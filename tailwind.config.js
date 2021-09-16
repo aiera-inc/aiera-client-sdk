7d1422f152a9c26fc3b097e7fd15295e3b96a8ce
Kevin LaFlamme <kevin@aiera.com>
2021-09-16 08:38:25 -0400

    Add [odd] utils for tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,7 @@ module.exports = {
     variants: {
         extend: {
             margin: ['first', 'last'],
+            backgroundColor: ['odd'],
         },
     },
     plugins: [require('@tailwindcss/line-clamp')],
