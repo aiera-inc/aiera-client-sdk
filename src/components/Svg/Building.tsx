17e2424cf6f1f476342fb93d740b5fdef799389c
Kevin LaFlamme <kevin@aiera.com>
2021-09-22 12:34:22 -0400

    Give BEM classname to SVGs

--- src/components/Svg/Building.tsx
+++ src/components/Svg/Building.tsx
@@ -1,7 +1,8 @@
 import React, { ReactElement } from 'react';
+import classNames from 'classnames';
 import { Svg } from '.';
 import building from '@aiera/client-sdk/assets/building.svg';
 
 export function Building({ className }: { className?: string }): ReactElement {
-    return <Svg alt="building" className={className} src={building} />;
+    return <Svg alt="building" className={classNames(className, 'Svg__building')} src={building} />;
 }
