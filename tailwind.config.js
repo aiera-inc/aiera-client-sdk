aff01ed42aa912cdcbedb9c8bbcc81e79284e862
Elliot Nash <elliot@venturemedias.com>
2021-09-22 21:31:55 -0400

    Starting Transcript Styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,13 +2,17 @@ module.exports = {
     purge: ['./src/**/*.html', './src/**/*.css', './src/**/*.ts*'],
     darkMode: false, // or 'media' or 'class'
     theme: {
-        extend: {},
+        extend: {
+            boxShadow: {
+                header: '0px 1px 2px rgba(0, 0, 0, 0.07), 0px 12px 24px rgba(0, 0, 0, 0.03)'
+            }
+        }
     },
     variants: {
         extend: {
             margin: ['first', 'last'],
-            backgroundColor: ['odd'],
-        },
+            backgroundColor: ['odd']
+        }
     },
-    plugins: [require('@tailwindcss/line-clamp')],
+    plugins: [require('@tailwindcss/line-clamp')]
 };
