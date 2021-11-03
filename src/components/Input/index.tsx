15fb351fde4b13b76190ad997161e5fe2c08748c
Kevin LaFlamme <kevin@aiera.com>
2021-11-03 12:00:08 -0400

    feat: add ability to clear input with clear button

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,6 +1,16 @@
-import React, { useCallback, ChangeEvent, ChangeEventHandler, FocusEventHandler, ReactElement, ReactNode } from 'react';
+import React, {
+    useCallback,
+    MouseEvent,
+    MouseEventHandler,
+    ChangeEvent,
+    ChangeEventHandler,
+    FocusEventHandler,
+    ReactElement,
+    ReactNode,
+} from 'react';
 import classNames from 'classnames';
 import { ChangeHandler } from '@aiera/client-sdk/types';
+import { Close } from '@aiera/client-sdk/components/Svg/Close';
 import './styles.css';
 
 interface InputSharedProps {
@@ -8,18 +18,19 @@ interface InputSharedProps {
     placeholder?: string;
     onFocus?: FocusEventHandler;
     value?: string;
-    defaultValue?: string;
     className?: string;
+    clearable?: boolean;
     name: string;
 }
 
 /** @notExported */
 interface InputUIProps extends InputSharedProps {
+    clear: MouseEventHandler<HTMLDivElement>;
     onChange: ChangeEventHandler<HTMLInputElement>;
 }
 
 export function InputUI(props: InputUIProps): ReactElement {
-    const { children, placeholder, onChange, onFocus, value, name, className = '', defaultValue } = props;
+    const { children, clearable, clear, placeholder, onChange, onFocus, value, name, className = '' } = props;
     return (
         <div className={`group h-8 items-center w-full relative ${className} input__${name}`}>
             <input
@@ -30,7 +41,6 @@ export function InputUI(props: InputUIProps): ReactElement {
                 onChange={onChange}
                 onFocus={onFocus}
                 placeholder={placeholder}
-                defaultValue={defaultValue}
                 value={value}
             />
             {React.isValidElement(children) && (
@@ -39,6 +49,14 @@ export function InputUI(props: InputUIProps): ReactElement {
                         className:
                             'group-focus-within:stroke-current group-focus-within:text-blue-600 z-1 relative w-4',
                     })}
+                </div>
+            )}
+            {clearable && value && (
+                <div
+                    className="h-full absolute flex-col items-center justify-center top-0 right-2 w-3 text-gray-300 cursor-pointer hidden group-hover:flex hover:text-gray-500 active:text-gray-700"
+                    onClick={clear}
+                >
+                    <Close />
                 </div>
             )}
         </div>
@@ -54,9 +72,14 @@ export interface InputProps extends InputSharedProps {
  * Renders Input
  */
 export function Input(props: InputProps): ReactElement {
-    const { children, placeholder, onChange, onFocus, value, name, className, defaultValue } = props;
+    const { children, clearable = true, placeholder, onChange, onFocus, value, name, className } = props;
     return (
         <InputUI
+            clearable={clearable}
+            clear={useCallback(
+                (event: MouseEvent<HTMLDivElement>) => onChange?.(event, { name, value: '' }),
+                [onChange]
+            )}
             placeholder={placeholder}
             onChange={useCallback(
                 (event: ChangeEvent<HTMLInputElement>) =>
@@ -64,7 +87,6 @@ export function Input(props: InputProps): ReactElement {
                 [onChange]
             )}
             onFocus={onFocus}
-            defaultValue={defaultValue}
             value={value}
             className={className}
             name={name}
--- src/components/Input/test.tsx
+++ src/components/Input/test.tsx
@@ -1,19 +1,19 @@
 import React from 'react';
 import { render, fireEvent, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 
 import { Input } from '.';
 
 describe('Input', () => {
     test('renders', () => {
-        render(<Input name="input" defaultValue="test" placeholder="example" />);
+        render(<Input name="input" value="test" placeholder="example" />);
         const inputEle = screen.getByPlaceholderText('example');
-        fireEvent.change(inputEle, { target: { value: 'changed' } });
-        expect(screen.getByPlaceholderText('example')).toHaveValue('changed');
+        expect(inputEle).toHaveValue('test');
     });
 
     test('onChange called', () => {
         const onChange = jest.fn();
-        render(<Input name="input" defaultValue="test" placeholder="example" onChange={onChange} />);
+        render(<Input name="input" value="test" placeholder="example" onChange={onChange} />);
         const inputEle = screen.getByPlaceholderText('example');
         fireEvent.change(inputEle, { target: { value: 'changed' } });
         expect(onChange).toHaveBeenCalledWith(expect.anything(), { name: 'input', value: 'changed' });
@@ -21,9 +21,27 @@ describe('Input', () => {
 
     test('onFocus called', () => {
         const onFocus = jest.fn();
-        render(<Input name="input" defaultValue="test" placeholder="example" onFocus={onFocus} />);
+        render(<Input name="input" value="test" placeholder="example" onFocus={onFocus} />);
         const inputEle = screen.getByPlaceholderText('example');
         fireEvent.focus(inputEle);
         expect(onFocus).toHaveBeenCalled();
+    });
+
+    test('can be cleared', () => {
+        const onChange = jest.fn();
+        render(<Input name="input" value="test" placeholder="example" onChange={onChange} />);
+        const inputEle = screen.getByPlaceholderText('example');
+        userEvent.hover(inputEle);
+        const clearButton = screen.getByTitle('Close');
+        userEvent.click(clearButton);
+        expect(onChange).toHaveBeenCalledWith(expect.anything(), expect.objectContaining({ name: 'input', value: '' }));
+    });
+
+    test('clear button can be skipped', () => {
+        const onChange = jest.fn();
+        render(<Input clearable={false} name="input" value="test" placeholder="example" onChange={onChange} />);
+        const inputEle = screen.getByPlaceholderText('example');
+        userEvent.hover(inputEle);
+        expect(screen.queryByTitle('Close')).toBeNull();
     });
 });
