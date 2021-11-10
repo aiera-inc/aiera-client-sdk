99758b524135ae52f317334827b5219c67028a74
Vich <mmichael0413@gmail.com>
2021-11-10 16:16:45 -0500

    test: ContentList results

--- src/modules/ContentList/test.tsx
+++ src/modules/ContentList/test.tsx
@@ -1,12 +1,25 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import { within } from '@testing-library/dom';
 
 import { ContentList } from '.';
 
 describe('ContentList', () => {
-    test('renders content tabs', () => {
+    test('renders tabs', () => {
         render(<ContentList />);
         screen.getByText('News');
         screen.getByText('Corp. Activity');
+    });
+
+    test('renders list', () => {
+        render(<ContentList />);
+        screen.getByText('GME');
+        const row = screen.getByText('GME').closest('li');
+        expect(row).toBeTruthy();
+        if (row) {
+            within(row).getByText('NYSE');
+            within(row).getByText('Oct 21, 2021');
+            within(row).getByText('Benzinga');
+        }
     });
 });
