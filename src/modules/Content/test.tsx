efb4b9054c8b7a74449ce34276886cafa8b5259b
Vich <mmichael0413@gmail.com>
2021-11-12 15:32:41 -0500

    refactor: use match pattern in Content, string values for ContentType enum, and update Content tests

--- src/modules/Content/test.tsx
+++ src/modules/Content/test.tsx
@@ -1,13 +1,38 @@
 import React from 'react';
-// import { screen } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 
-import { ContentType } from '@aiera/client-sdk/modules/ContentList';
-import { renderWithClient } from 'testUtils';
+import { ContentListContent, ContentType, mockBody } from '@aiera/client-sdk/modules/ContentList';
 import { Content } from '.';
 
+const content: ContentListContent = {
+    id: 1,
+    date: '2021-10-21',
+    companyIdentifier: 'GME',
+    exchangeName: 'NYSE',
+    sourceName: 'Benzinga',
+    title: 'Famous Stonk Stonking Again As Retail Investors (aka Ape) Stop Eyeing the Moon and Shoot for Andromeda',
+};
+
 describe('Content', () => {
-    test('renders', () => {
-        renderWithClient(<Content contentType={ContentType.news} />);
-        // screen.getByText('ContentUI');
+    test('renders content', () => {
+        render(
+            <Content
+                body={mockBody}
+                companyIdentifier={content.companyIdentifier}
+                contentType={ContentType.news}
+                date={content.date}
+                exchangeName={content.exchangeName}
+                onBack={jest.fn()}
+                sourceName={content.sourceName}
+                title={content.title}
+            />
+        );
+        screen.getByText('News');
+        screen.getByText('GME');
+        screen.getByText('NYSE');
+        screen.getByText('Famous Stonk', { exact: false });
+        screen.getByText('Benzinga');
+        screen.getByText('Oct 21, 2021');
+        screen.getByText('Netflix, Inc.', { exact: false });
     });
 });
