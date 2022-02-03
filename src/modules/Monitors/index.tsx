e42cadd7cb29c354cdd615db414c9fffb1ba5c3b
Elliot Nash <elliot@venturemedias.com>
2022-02-03 16:10:26 -0500

    More ui in monitors

--- src/modules/Monitors/index.tsx
+++ src/modules/Monitors/index.tsx
@@ -1,6 +1,7 @@
 import { Button } from '@aiera/client-sdk/components/Button';
 import { CompanyFilterButton } from '@aiera/client-sdk/components/CompanyFilterButton';
 import { SettingsButton } from '@aiera/client-sdk/components/SettingsButton';
+import { Gear } from '@aiera/client-sdk/components/Svg/Gear';
 import { Plus } from '@aiera/client-sdk/components/Svg/Plus';
 import classNames from 'classnames';
 import React, { ReactElement, ReactNode } from 'react';
@@ -12,6 +13,24 @@ interface MonitorsSharedProps {
 
 /** @notExported */
 interface MonitorsUIProps extends MonitorsSharedProps {}
+
+function Monitor() {
+    return (
+        <div className="flex flex-col bg-white mb-3 px-3 py-2 shadow-sm shadow-slate-200 rounded-lg">
+            <div className="flex justify-between items-start">
+                <div className="flex flex-col pr-2 overflow-hidden">
+                    <p className="text-lg font-medium antialiased">Self Driving</p>
+                    <p className="text-sm truncate text-slate-400">
+                        Tesla, Elon Musk, Self Driving, Autonomous, Chevy Cruise, another one
+                    </p>
+                </div>
+                <div className="flex h-6">
+                    <Gear className="w-4" />
+                </div>
+            </div>
+        </div>
+    );
+}
 
 export function MonitorsUI(props: MonitorsUIProps): ReactElement {
     const { children } = props;
@@ -32,7 +51,7 @@ export function MonitorsUI(props: MonitorsUIProps): ReactElement {
                 </div>
             </div>
             <div className="bg-slate-50 p-3 flex justify-between items-center">
-                <div className="flex justify-between items-baseline text-slate-400">
+                <div className="flex justify-between items-baseline text-slate-400 pl-1">
                     <p className="text-sm font-semibold text-slate-900">1 Week</p>
                     <p className="text-sm mx-4">1 Month</p>
                     <p className="text-sm">1 Year</p>
@@ -40,21 +59,15 @@ export function MonitorsUI(props: MonitorsUIProps): ReactElement {
                 <CompanyFilterButton />
             </div>
             <div className="bg-slate-50 px-3 pb-3 flex-1 flex flex-col relative overflow-y-auto">
-                <div className="bg-white mb-3 p-3 shadow-sm shadow-slate-200 rounded-lg">
-                    <p>Stuff</p>
-                </div>
-                <div className="bg-white mb-3 p-3 shadow-sm shadow-slate-200 rounded-lg">
-                    <p>Stuff</p>
-                </div>
-                <div className="bg-white mb-3 p-3 shadow-sm shadow-slate-200 rounded-lg">
-                    <p>Stuff</p>
-                </div>
-                <div className="bg-white mb-3 p-3 shadow-sm shadow-slate-200 rounded-lg">
-                    <p>Stuff</p>
-                </div>
-                <div className="bg-white mb-3 p-3 shadow-sm shadow-slate-200 rounded-lg">
-                    <p>Stuff</p>
-                </div>
+                <Monitor />
+                <Monitor />
+                <Monitor />
+                <Monitor />
+                <Monitor />
+                <Monitor />
+                <Monitor />
+                <Monitor />
+                <Monitor />
                 {children || 'MonitorsUI'}
             </div>
         </div>
