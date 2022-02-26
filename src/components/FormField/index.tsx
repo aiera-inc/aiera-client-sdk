eb08318f75b295f7ae18cbf9cdab7b70d8bce2f1
Vich <mmichael0413@gmail.com>
2022-02-25 23:08:16 -0500

    refactor(FormField): export the input and select components separately and just render children in FormField

--- src/components/FormField/index.tsx
+++ src/components/FormField/index.tsx
@@ -1,6 +1,5 @@
-import React, { ReactElement } from 'react';
+import React, { ReactElement, ReactNode } from 'react';
 import classNames from 'classnames';
-import { match } from 'ts-pattern';
 
 import { Checkbox } from '@aiera/client-sdk/components/Checkbox';
 import { Input } from '@aiera/client-sdk/components/Input';
@@ -13,26 +12,30 @@ type FormFieldSelectOption = {
     value: string;
 };
 
-interface FormFieldSharedProps {
-    autoFocus?: boolean;
+interface SharedProps {
     className?: string;
-    description?: string;
-    label?: string;
     name: string;
     onChange?: ChangeHandler<string>;
-    options?: FormFieldSelectOption[];
-    placeholder?: string;
-    type?: 'input' | 'select';
     value: string;
 }
 
-/** @notExported */
-interface FormFieldUIProps extends FormFieldSharedProps {}
+interface FormFieldSharedProps {
+    children?: ReactNode;
+    className?: string;
+}
 
-function FormFieldInputUI(props: FormFieldUIProps) {
-    const { autoFocus, description, label, name, onChange, placeholder, value } = props;
+/** @notExported */
+export interface FormFieldInputProps extends SharedProps {
+    autoFocus?: boolean;
+    description?: string;
+    label?: string;
+    placeholder?: string;
+}
+
+export function FormFieldInput(props: FormFieldInputProps) {
+    const { autoFocus, className, description, label, name, onChange, placeholder, value } = props;
     return (
-        <>
+        <FormField className={className}>
             {!!label && <p className="font-semibold text-base text-black form-field__label">{label}</p>}
             {!!description && (
                 <p className="font-light leading-4 pt-0.5 text-[#ABB2C7] text-sm  form-field__description">
@@ -49,19 +52,19 @@ function FormFieldInputUI(props: FormFieldUIProps) {
                     value={value}
                 />
             </div>
-        </>
+        </FormField>
     );
 }
 
 /** @notExported */
-interface FormFieldSelectProps extends FormFieldUIProps {
+export interface FormFieldSelectProps extends SharedProps {
     options: FormFieldSelectOption[];
 }
 
-function FormFieldSelectUI(props: FormFieldSelectProps): ReactElement {
-    const { name, onChange, options, value } = props;
+export function FormFieldSelect(props: FormFieldSelectProps): ReactElement {
+    const { className, name, onChange, options, value } = props;
     return (
-        <>
+        <FormField className={className}>
             {options.map((option) => (
                 <div
                     className="border-b border-gray-100 cursor-pointer flex h[70px] items-center px-4 py-3 hover:bg-gray-50 first:hover:rounded-t last:hover:rounded-b last:border-0"
@@ -84,45 +87,16 @@ function FormFieldSelectUI(props: FormFieldSelectProps): ReactElement {
                     <Checkbox checked={value === option.value} className="ml-auto flex-shrink-0" />
                 </div>
             ))}
-        </>
+        </FormField>
     );
 }
 
+/** @notExported */
+interface FormFieldUIProps extends FormFieldSharedProps {}
+
 export function FormFieldUI(props: FormFieldUIProps): ReactElement {
-    const {
-        autoFocus,
-        className = 'form-field',
-        description,
-        label,
-        onChange,
-        placeholder,
-        name,
-        options,
-        type = 'input',
-        value,
-    } = props;
-    return (
-        <div className={`bg-white border border-gray-200 px-4 py-3 rounded shadow-xl ${className}`}>
-            {match(type)
-                .with('input', () => (
-                    <FormFieldInputUI
-                        autoFocus={autoFocus}
-                        description={description}
-                        label={label}
-                        name={name}
-                        onChange={onChange}
-                        placeholder={placeholder}
-                        value={value}
-                    />
-                ))
-                .with('select', () =>
-                    options ? (
-                        <FormFieldSelectUI name={name} onChange={onChange} options={options} value={value} />
-                    ) : null
-                )
-                .otherwise(() => null)}
-        </div>
-    );
+    const { className = 'form-field', children } = props;
+    return <div className={`bg-white border border-gray-200 px-4 py-3 rounded shadow-xl ${className}`}>{children}</div>;
 }
 
 /** @notExported */
@@ -132,19 +106,6 @@ export interface FormFieldProps extends FormFieldSharedProps {}
  * Renders FormField
  */
 export function FormField(props: FormFieldProps): ReactElement {
-    const { autoFocus, className, description, label, name, onChange, options, placeholder, type, value } = props;
-    return (
-        <FormFieldUI
-            autoFocus={autoFocus}
-            className={className}
-            description={description}
-            label={label}
-            name={name}
-            onChange={onChange}
-            options={options}
-            placeholder={placeholder}
-            type={type}
-            value={value}
-        />
-    );
+    const { children } = props;
+    return <FormFieldUI>{children}</FormFieldUI>;
 }
