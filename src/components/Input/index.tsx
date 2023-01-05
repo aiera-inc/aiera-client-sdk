69c2e62828d6747b67128c802d712e4a056a7d56
Vich <mmichael0413@gmail.com>
2023-01-05 11:33:43 -0500

    chore(Input): fix container styles

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -55,53 +55,51 @@ export function InputUI(props: InputUIProps): ReactElement {
         value,
     } = props;
     return (
-        <div className={`flex flex-col relative ${className} input__${name}`}>
+        <div className={`group h-8 items-center w-full relative dark:text-white ${className} input__${name}`}>
             {error && <div className="absolute bottom-8 right-0 text-red-600 text-sm z-10">{error}</div>}
-            <div className="group h-8 items-center w-full relative dark:text-white">
-                {React.isValidElement(icon) && (
-                    <div className="absolute pointer-events-none h-8 w-8 justify-center items-center flex">
-                        {React.cloneElement(icon, {
-                            className: `group-focus-within:stroke-current group-focus-within:text-${
-                                error ? 'red' : 'blue'
-                            }-600 z-1 relative w-4`,
-                        })}
-                    </div>
+            {React.isValidElement(icon) && (
+                <div className="absolute pointer-events-none h-8 w-8 justify-center items-center flex">
+                    {React.cloneElement(icon, {
+                        className: `group-focus-within:stroke-current group-focus-within:text-${
+                            error ? 'red' : 'blue'
+                        }-600 z-1 relative w-4`,
+                    })}
+                </div>
+            )}
+            <input
+                autoFocus={autoFocus}
+                className={classNames(
+                    'w-full h-full text-sm border border-gray-200 rounded-lg focus:border-1 focus:outline-none dark:bg-bluegray-6 dark:border-bluegray-5',
+                    {
+                        'pl-7': !!icon,
+                        'pl-3': !icon,
+                        'border-red-600': !!error,
+                        'focus:border-blue-600': !error,
+                        'focus:border-red-600': !!error,
+                        'focus:shadow-input': !error,
+                        'focus:shadow-inputError': !!error,
+                        'hover:border-blue-400': !error,
+                        'hover:border-red-400': !!error,
+                    }
                 )}
-                <input
-                    autoFocus={autoFocus}
-                    className={classNames(
-                        'w-full h-full text-sm border border-gray-200 rounded-lg focus:border-1 focus:outline-none dark:bg-bluegray-6 dark:border-bluegray-5',
-                        {
-                            'pl-7': !!icon,
-                            'pl-3': !icon,
-                            'border-red-600': !!error,
-                            'focus:border-blue-600': !error,
-                            'focus:border-red-600': !!error,
-                            'focus:shadow-input': !error,
-                            'focus:shadow-inputError': !!error,
-                            'hover:border-blue-400': !error,
-                            'hover:border-red-400': !!error,
-                        }
-                    )}
-                    id={id}
-                    name={name}
-                    onBlur={onBlur}
-                    onChange={onChange}
-                    onFocus={onFocus}
-                    placeholder={placeholder}
-                    ref={inputRef}
-                    value={value}
-                    type={type}
-                />
-                {clearable && value && (
-                    <div
-                        className="h-full absolute flex-col items-center justify-center top-0 right-2 w-3 text-gray-300 cursor-pointer hidden group-hover:flex hover:text-gray-500 active:text-gray-700"
-                        onClick={clear}
-                    >
-                        <Close />
-                    </div>
-                )}
-            </div>
+                id={id}
+                name={name}
+                onBlur={onBlur}
+                onChange={onChange}
+                onFocus={onFocus}
+                placeholder={placeholder}
+                ref={inputRef}
+                value={value}
+                type={type}
+            />
+            {clearable && value && (
+                <div
+                    className="h-full absolute flex-col items-center justify-center top-0 right-2 w-3 text-gray-300 cursor-pointer hidden group-hover:flex hover:text-gray-500 active:text-gray-700"
+                    onClick={clear}
+                >
+                    <Close />
+                </div>
+            )}
         </div>
     );
 }
