9b16532f3cafb53bf253c20f98b52518f8e6b500
Elliot Nash <elliot@venturemedias.com>
2023-02-28 11:13:28 -0500

    Feat: start aieracast component

--- src/lib/config/index.tsx
+++ src/lib/config/index.tsx
@@ -6,7 +6,7 @@ import { Options as RealtimeOptions } from 'pusher-js';
 import { defaultEnv } from '@aiera/client-sdk/lib/config/env';
 import { useMessageListener } from '../msg';
 
-type Module = 'ASR' | 'EventList' | 'NewsList' | 'EventByTicker' | 'EventListByTicker';
+type Module = 'Aieracast' | 'ASR' | 'EventList' | 'NewsList' | 'EventByTicker' | 'EventListByTicker';
 type Platform = 'aiera-sdk-dev' | 'embedded' | 'eze-eclipse' | 'glue42' | 'finsemble' | 'openfin';
 
 export type EventListFilterType = 'earningsOnly' | 'transcripts';
