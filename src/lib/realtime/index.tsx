63eb856b0a58b0ac678d60b108de12f7279fc97c
Vich <mmichael0413@gmail.com>
2023-07-20 17:47:41 -0400

    fix: refetch current user and app config queries until both are set before setting Pusher client

--- src/lib/realtime/index.tsx
+++ src/lib/realtime/index.tsx
@@ -1,9 +1,15 @@
 import React, { createContext, useEffect, useState, useContext, ReactNode, ReactElement } from 'react';
+import gql from 'graphql-tag';
 import Pusher from 'pusher-js';
 import merge from 'lodash.merge';
 
+import { useQuery } from '@aiera/client-sdk/api/client';
 import { useConfig } from '@aiera/client-sdk/lib/config';
 import { useAppConfig } from '@aiera/client-sdk/lib/data';
+import { AppConfigQuery, RealtimeCurrentUserQuery } from '@aiera/client-sdk/types/generated';
+
+type AppConfiguration = AppConfigQuery['configuration'];
+type RealtimeCurrentUser = RealtimeCurrentUserQuery['currentUser'];
 
 export type Realtime = Pusher;
 
@@ -11,15 +17,60 @@ export const Context = createContext<Realtime | undefined>(undefined);
 
 export function Provider({ children, client: passedClient }: { children: ReactNode; client?: Pusher }): ReactElement {
     const [client, setClient] = useState<Realtime | undefined>(passedClient);
-    const configQuery = useAppConfig();
-    const { realtimeOptions } = useConfig();
+
+    // Keep current user and Pusher config in state
+    // If the config isn't set, wait until user is authed to refetch
+    // And only then set the Pusher client
+    const [appConfig, setAppConfig] = useState<AppConfiguration | undefined>(undefined);
+    const [currentUser, setCurrentUser] = useState<RealtimeCurrentUser | undefined>(undefined);
+
+    // Auth needed to retrieve Pusher config from server
+    const userQuery = useQuery<RealtimeCurrentUserQuery>({
+        requestPolicy: 'cache-only',
+        query: gql`
+            query RealtimeCurrentUser {
+                currentUser {
+                    id
+                }
+            }
+        `,
+    });
     useEffect(() => {
-        const appKey = configQuery.state.data?.configuration?.pusherAppKey;
-        const cluster = configQuery.state.data?.configuration?.pusherAppCluster;
-        if (!passedClient && appKey && cluster) {
+        if (!currentUser) {
+            if (userQuery.status === 'success' && userQuery.state.data?.currentUser?.id) {
+                setCurrentUser(userQuery.state.data.currentUser);
+            } else if (
+                userQuery.status === 'error' ||
+                (userQuery.status === 'success' && !userQuery.state.data?.currentUser?.id)
+            ) {
+                userQuery.refetch({ requestPolicy: 'cache-and-network' });
+            }
+        }
+    }, [currentUser, userQuery.state.data?.currentUser, userQuery.status]);
+
+    const configQuery = useAppConfig();
+    useEffect(() => {
+        if (!appConfig && currentUser) {
+            if (configQuery.status === 'success' && configQuery.state.data?.configuration) {
+                setAppConfig(configQuery.state.data.configuration);
+            } else if (
+                configQuery.status === 'error' ||
+                (configQuery.status === 'success' && !configQuery.data?.configuration)
+            ) {
+                configQuery.refetch();
+            }
+        }
+    }, [appConfig, configQuery.state.data?.configuration, configQuery.status, currentUser]);
+
+    const { realtimeOptions } = useConfig();
+    console.log({ appConfig, currentUser, client });
+    useEffect(() => {
+        const appKey = appConfig?.pusherAppKey;
+        const cluster = appConfig?.pusherAppCluster;
+        if (!client && !passedClient && appKey && cluster) {
             setClient(new Pusher(appKey, merge({ cluster }, realtimeOptions)));
         }
-    }, [configQuery.state.data?.configuration?.pusherAppKey, configQuery.state.data?.configuration?.pusherAppKey]);
+    }, [appConfig, client, passedClient]);
     return <Context.Provider value={client}>{children}</Context.Provider>;
 }
 
