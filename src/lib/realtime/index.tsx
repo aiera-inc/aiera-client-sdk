0620b63b7ed6e54a37a7dcbcd09660f5c693e9aa
Vich <mmichael0413@gmail.com>
2023-12-12 15:56:54 -0500

    chore: refactor current user refetch polling

--- src/lib/realtime/index.tsx
+++ src/lib/realtime/index.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useEffect, useState, useContext, ReactNode, ReactElement } from 'react';
+import React, { createContext, useContext, useEffect, useState, ReactNode, ReactElement } from 'react';
 import gql from 'graphql-tag';
 import Pusher from 'pusher-js';
 import merge from 'lodash.merge';
@@ -8,10 +8,10 @@ import { useConfig } from '@aiera/client-sdk/lib/config';
 import { useAppConfig } from '@aiera/client-sdk/lib/data';
 import { AppConfigQuery, RealtimeCurrentUserQuery } from '@aiera/client-sdk/types/generated';
 
-const REFETCH_LIMIT = 100; // after 100 failed attempts, stop querying
+const REFETCH_LIMIT = 1000; // after 1000 failed attempts, stop querying
+const REFETCH_THRESHOLD = 100; // after 100 failed attempts, query every 30 seconds
 const REFETCH_TIMEOUT_DEFAULT = 500; // in ms
-const REFETCH_TIMEOUT_LIMIT = 2000; // 2 seconds (in ms)
-const REFETCH_TIMEOUT_THRESHOLD = 30;
+const REFETCH_TIMEOUT_DELAYED = 30000; // 30 seconds (in ms)
 
 type AppConfiguration = AppConfigQuery['configuration'];
 type RealtimeCurrentUser = RealtimeCurrentUserQuery['currentUser'];
@@ -22,6 +22,7 @@ export const Context = createContext<Realtime | undefined>(undefined);
 
 export function Provider({ children, client: passedClient }: { children: ReactNode; client?: Pusher }): ReactElement {
     const [client, setClient] = useState<Realtime | undefined>(passedClient);
+    let userQueryTimer: number | undefined;
 
     /**
      * Keep current user and Pusher config in state.
@@ -45,9 +46,24 @@ export function Provider({ children, client: passedClient }: { children: ReactNo
             }
         `,
     });
-    useEffect(() => {
-        let refetchTimer: number | undefined;
 
+    function userQueryRefetch() {
+        userQuery.refetch({ requestPolicy: 'cache-and-network' });
+        setRefetchCount(refetchCount + 1);
+    }
+
+    function startUserQueryPolling(refetchCount: number) {
+        if (refetchCount < REFETCH_LIMIT) {
+            userQueryTimer = window.setTimeout(
+                userQueryRefetch,
+                refetchCount < REFETCH_THRESHOLD ? REFETCH_TIMEOUT_DEFAULT : REFETCH_TIMEOUT_DELAYED
+            );
+        } else if (userQueryTimer) {
+            clearTimeout(userQueryTimer);
+        }
+    }
+
+    useEffect(() => {
         if (!currentUser) {
             if (userQuery.status === 'success' && userQuery.state.data?.currentUser?.id) {
                 setCurrentUser(userQuery.state.data.currentUser);
@@ -56,27 +72,14 @@ export function Provider({ children, client: passedClient }: { children: ReactNo
                 (userQuery.status === 'success' && !userQuery.state.data?.currentUser?.id)
             ) {
                 /**
+                 * Poll the server on an interval to get the current user.
                  * If there's an issue, don't hammer the server.
-                 * Once the threshold is hit, refetch every 2 seconds.
+                 * Once the threshold is hit, refetch every 30 seconds.
                  * And once the limit is reached, stop refetching entirely.
                  */
-                if (refetchCount < REFETCH_LIMIT) {
-                    refetchTimer = window.setTimeout(
-                        () => {
-                            setRefetchCount(refetchCount + 1);
-                            userQuery.refetch({ requestPolicy: 'cache-and-network' });
-                        },
-                        refetchCount < REFETCH_TIMEOUT_THRESHOLD ? REFETCH_TIMEOUT_DEFAULT : REFETCH_TIMEOUT_LIMIT
-                    );
-                }
+                startUserQueryPolling(refetchCount);
             }
         }
-        // Remove refetch timeout on cleanup
-        return () => {
-            if (refetchTimer) {
-                window.clearTimeout(refetchTimer);
-            }
-        };
     }, [currentUser, refetchCount, userQuery.state.data?.currentUser, userQuery.status]);
 
     const configQuery = useAppConfig();
