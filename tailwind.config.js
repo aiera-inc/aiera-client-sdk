56e3d0464449c11944114dd0a60936ec33f1ee5e
Elliot Nash <elliot@venturemedias.com>
2024-10-02 16:52:45 -0400

    feature: chat header search

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,4 @@
 module.exports = {
-    mode: 'jit',
     content: ['./src/**/*.html', './src/**/*.css', './src/**/*.ts*'],
     darkMode: 'class', // or 'media' or 'class'
     theme: {
@@ -41,20 +40,6 @@ module.exports = {
                 grab: 'grab',
                 grabbing: 'grabbing',
             },
-        },
-    },
-    variants: {
-        extend: {
-            boxShadow: ['hover', 'active', 'focus', 'focus-within'],
-            backgroundColor: ['odd', 'active'],
-            border: ['focus', 'focus-within', 'hover', 'active'],
-            ringColor: ['focus', 'focus-within', 'hover', 'active'],
-            fill: ['hover', 'active', 'group-active', 'group-focus-within'],
-            stroke: ['hover', 'active', 'group-active', 'group-focus-within'],
-            margin: ['first', 'last'],
-            opacity: ['disabled'],
-            padding: ['first', 'last'],
-            textColor: ['hover', 'active', 'group-active', 'group-focus-within'],
         },
     },
     plugins: [require('@tailwindcss/line-clamp'), require('tailwindcss-interaction-variants')],
