112e1963406a0eb5601fa8b53dfb7ad9a433b102
Elliot Nash <elliot@venturemedias.com>
2024-11-05 12:24:29 -0500

    progress:Install VirtuosoMessageList

--- src/modules/AieraChat/test.tsx
+++ src/modules/AieraChat/test.tsx
@@ -1,22 +1,31 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import { AieraChat, AieraChatUI } from '.';
+import { Header } from './Header';
+import { Sources } from './Sources';
 
 describe('AieraChat', () => {
-    it('renders AieraChatUI', () => {
-        render(<AieraChat />);
+    it('Header render title', () => {
+        render(
+            <Header
+                title="My New Chat"
+                onOpenMenu={function (): void {
+                    throw new Error('Function not implemented.');
+                }}
+                onOpenSources={function (): void {
+                    throw new Error('Function not implemented.');
+                }}
+            />
+        );
         expect(screen.getByText('My New Chat')).toBeInTheDocument();
     });
-});
-
-describe('AieraChatUI', () => {
-    it('renders the header with correct title', () => {
-        render(<AieraChatUI />);
-        expect(screen.getByText('My New Chat')).toBeInTheDocument();
-    });
-
-    it('renders chat and footer placeholders', () => {
-        render(<AieraChatUI />);
-        expect(screen.getByText('chat')).toBeInTheDocument();
+    it('Sources panel', () => {
+        render(
+            <Sources
+                onClose={function (): void {
+                    throw new Error('Function not implemented.');
+                }}
+            />
+        );
+        expect(screen.getByText('Sources')).toBeInTheDocument();
     });
 });
