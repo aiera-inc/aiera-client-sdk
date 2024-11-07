3ef3985335d6d8bf35c206c32292920d8300db8d
Elliot Nash <elliot@venturemedias.com>
2024-11-07 00:24:25 -0500

    progress: starting sticky headers

--- src/modules/AieraChat/Messages/index.tsx
+++ src/modules/AieraChat/Messages/index.tsx
@@ -3,9 +3,11 @@ import {
     VirtuosoMessageListLicense,
     VirtuosoMessageListMethods,
     VirtuosoMessageListProps,
+    useCurrentlyRenderedData,
 } from '@virtuoso.dev/message-list';
 import React, { useCallback, useRef } from 'react';
 import { Prompt } from '../Prompt';
+import './styles.css';
 
 interface Message {
     key: string;
@@ -18,6 +20,19 @@ let idCounter = 0;
 function randomMessage(user: Message['user']): Message {
     return { user, key: `${idCounter++}`, text: 'some other message' };
 }
+
+const StickyHeader: VirtuosoMessageListProps<Message, null>['StickyHeader'] = () => {
+    const firstItem = useCurrentlyRenderedData<{ text: string }>()[0] as { text: string } | undefined;
+    return (
+        <div style={{ width: '100%', position: 'absolute', top: 0 }}>
+            <div style={{ textAlign: 'center', fontWeight: 300 }}>
+                <span style={{ backgroundColor: '#F0F0F3', padding: '0.1rem 2rem', borderRadius: '0.5rem' }}>
+                    {firstItem?.text}
+                </span>
+            </div>
+        </div>
+    );
+};
 
 const ItemContent: VirtuosoMessageListProps<Message, null>['ItemContent'] = ({ data }: { data: Message }) => {
     const ownMessage = data.user === 'me';
@@ -73,15 +88,17 @@ export function Messages() {
 
     return (
         <div className="relative flex-1">
-            <div className="absolute inset-0 flex flex-col flex-1">
+            <div className="absolute bottom-0 left-0 right-0 top-4 flex flex-col flex-1">
                 <VirtuosoMessageListLicense licenseKey="">
                     <VirtuosoMessageList<Message, null>
                         ref={virtuoso}
                         style={{ flex: 1 }}
                         computeItemKey={({ data }: { data: Message }) => data.key}
+                        className="px-4 messagesScrollBars"
                         initialLocation={{ index: 'LAST', align: 'end' }}
                         shortSizeAlign="bottom-smooth"
                         ItemContent={ItemContent}
+                        StickyHeader={StickyHeader}
                     />
                 </VirtuosoMessageListLicense>
                 <Prompt onSubmit={onSubmit} />
