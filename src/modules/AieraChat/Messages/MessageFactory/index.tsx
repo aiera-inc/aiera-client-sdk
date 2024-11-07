19b6eb8aa5b3d97822d2cb50ef234a1042d07119
Elliot Nash <elliot@venturemedias.com>
2024-11-07 14:40:33 -0500

    Chat sticky fix

--- src/modules/AieraChat/Messages/MessageFactory/index.tsx
+++ src/modules/AieraChat/Messages/MessageFactory/index.tsx
@@ -5,21 +5,16 @@ import { MicroQuestionMark } from '@aiera/client-sdk/components/Svg/MicroQuestio
 
 export const MessagePrompt = ({ data }: { data: Message }) => {
     return (
-        <div className="px-4 py-3.5 bg-[#ECF0F3] rounded-xl flex items-center z-10 relative">
-            <div className="h-7 flex-shrink-0 w-7 mr-2.5 bg-indigo-600 flex items-center justify-center rounded-lg">
-                <MicroQuestionMark className="w-4 text-white" />
-            </div>
-            <div>
-                <p className="text-base font-bold antialiased leading-4">{data.prompt}</p>
-            </div>
+        <div>
+            <p className="text-base leading-[14px] font-bold antialiased">{data.prompt}</p>
         </div>
     );
 };
 
 const ItemContent = ({ data }: { data: Message }) => {
     return (
-        <div style={{ paddingBottom: '2rem', display: 'flex' }}>
-            <div className="p-4 text-base">{data.text}</div>
+        <div style={{ paddingBottom: '2rem' }}>
+            <div className="text-base leading-[14px]">{data.text}</div>
         </div>
     );
 };
