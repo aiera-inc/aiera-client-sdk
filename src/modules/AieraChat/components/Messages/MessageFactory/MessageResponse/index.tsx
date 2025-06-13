c8f737f1e3f714bbb10bbf6c430c20ce71b6cfc6
Vich <mmichael0413@gmail.com>
2025-06-13 16:02:05 -0400

    Smaller bottom-padding for message responses

--- src/modules/AieraChat/components/Messages/MessageFactory/MessageResponse/index.tsx
+++ src/modules/AieraChat/components/Messages/MessageFactory/MessageResponse/index.tsx
@@ -27,7 +27,7 @@ export const MessageResponse = ({ data, onReRun }: { onReRun: (k: string) => voi
     return data.status === ChatMessageStatus.PENDING || data.status === ChatMessageStatus.QUEUED ? (
         <Loading>Thinking...</Loading>
     ) : (
-        <div className="pb-10 flex flex-col">
+        <div className="pb-8 flex flex-col">
             <div className="flex flex-col px-4 pb-2">
                 {data.blocks?.map((block, index) => (
                     <Block {...block} key={index} />
